782e1af29952c5df8ebc588dcb44c47d537108d1
ZhechoZhechev <mishomishev024@gmail.com>
2022-11-03 13:57:34 +0200

    Lab Abstraction

---
+++ Lab_Abstraction/02.Cars/Seat.cs
@@ -0,0 +1,34 @@
+﻿namespace Cars
+{
+    using System;
+    class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+
+        public string Model => this.model;
+
+        public string Color => this.color;
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}{Environment.NewLine}{Start()}" +
+                $"{Environment.NewLine}{Stop()}";
+        }
+    }
+}
---
+++ Lab_Abstraction/02.Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿namespace Cars
+{
+    using System;
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model, color;
+        private int battery;
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.model = model;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        public string Model => this.model;
+
+        public string Color => this.color;
+
+        public int Battery => this.battery;
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries{Environment.NewLine}{Start()}" +
+                $"{Environment.NewLine}{Stop()}";
+        }
+    }
+}
