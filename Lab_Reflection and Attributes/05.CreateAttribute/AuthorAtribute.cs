03931683388b3fd09e37edf5aae73d81afd8a657
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-14 13:37:10 +0300

    creating attribute

--- Lab_Reflection and Attributes/05.CreateAttribute/AuthorAtribute.cs
+++ Lab_Reflection and Attributes/05.CreateAttribute/AuthorAtribute.cs
@@ -5,11 +5,11 @@
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class AuthorAttribute : Attribute
     {
-        public string Name { get; private set; }
-
         public AuthorAttribute(string name)
         {
             this.Name = name;
         }
+
+        public string Name { get; set; }
     }
 }
