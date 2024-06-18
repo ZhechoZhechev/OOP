257cd21f6bfee7b17696af3bc1319f4d45ad4819
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-18 16:18:11 +0300

    base class TeamMember added

---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/TheContentDepartment/Models/TeamMember.cs
@@ -0,0 +1,56 @@
+﻿namespace TheContentDepartment.Models;
+
+using TheContentDepartment.Models.Contracts;
+using TheContentDepartment.Utilities.Messages;
+
+public abstract class TeamMember : ITeamMember
+{
+    private string name = null!;
+    private string path = null!;
+    private List<string> inProgress;
+
+    protected TeamMember(string name, string path)
+    {
+        this.Name = name;
+        this.Path = path;
+        this.inProgress = new List<string>();
+    }
+    public string Name
+    {
+        get => this.name;
+
+        private set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(ExceptionMessages.NameNullOrWhiteSpace);
+            }
+
+            this.name = value;
+        }
+    }
+
+    public IReadOnlyCollection<string> InProgress => this.inProgress.AsReadOnly();
+    public string Path 
+    { 
+        get => this.path;
+
+        protected set 
+        {
+            this.path = value;
+        } 
+    }
+
+    public void FinishTask(string resourceName)
+    {
+        this.inProgress.Remove(resourceName);
+    }
+
+    public void WorkOnTask(string resourceName)
+    {
+        if (this.InProgress.Contains(resourceName))
+        {
+            this.inProgress.Add(resourceName);
+        }
+    }
+}
