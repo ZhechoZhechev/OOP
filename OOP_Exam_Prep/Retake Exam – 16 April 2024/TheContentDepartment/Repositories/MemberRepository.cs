8798bdd3c5d261f8358e0bc11f498b76f12e98a8
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-19 09:35:13 +0300

    MemberRepo

---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/TheContentDepartment/Repositories/MemberRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace TheContentDepartment.Repositories;
+
+using System.Collections.Generic;
+
+using Models.Contracts;
+using Repositories.Contracts;
+public class MemberRepository : IRepository<ITeamMember>
+{
+    private readonly List<ITeamMember> members = new List<ITeamMember>();
+
+    public IReadOnlyCollection<ITeamMember> Models => this.members.AsReadOnly();
+
+    public void Add(ITeamMember model)
+    {
+        this.members.Add(model);
+    }
+
+    public ITeamMember TakeOne(string modelName)
+    {
+        return this.members.FirstOrDefault(x => x.Name == modelName)!;
+    }
+}
