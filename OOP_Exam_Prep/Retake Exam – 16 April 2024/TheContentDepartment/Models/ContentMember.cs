ad8d2d97458808ad2578cdf0853a110027c074bd
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-19 09:02:12 +0300

    TeamMember subclasses added

---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/TheContentDepartment/Models/ContentMember.cs
@@ -0,0 +1,20 @@
+﻿namespace TheContentDepartment.Models;
+
+using TheContentDepartment.Utilities.Messages;
+
+public class ContentMember : TeamMember
+{
+    private readonly string[] pathOptions = new string[] { "CSharp", "JavaScript", "Python", "Java" };
+    public ContentMember(string name, string path) : base(name, path)
+    {
+        if (!pathOptions.Contains(path))
+        {
+            throw new ArgumentException(ExceptionMessages.PathIncorrect);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} - {this.Path} path. Currently working on {this.InProgress.Count()} tasks.";
+    }
+}
---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/TheContentDepartment/Models/TeamLead.cs
@@ -0,0 +1,19 @@
+﻿namespace TheContentDepartment.Models;
+
+using TheContentDepartment.Utilities.Messages;
+
+public class TeamLead : TeamMember
+{
+    public TeamLead(string name, string path) : base(name, path)
+    {
+        if (path != "Master")
+        {
+            throw new ArgumentException(ExceptionMessages.PathIncorrect);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} ({GetType().Name}) – Currently working on {this.InProgress.Count()} tasks.";
+    }
+}
