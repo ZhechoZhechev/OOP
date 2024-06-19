c53e79b63870a3397480ebca611b68801b03760a
ZhechoZhechev <mishomishev024@gmail.com>
2024-06-19 09:25:21 +0300

    ResourceRepo

---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/TheContentDepartment/Repositories/ResourceRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace TheContentDepartment.Repositories;
+
+using System.Collections.Generic;
+
+using Models.Contracts;
+using Repositories.Contracts;
+
+public class ResourceRepository : IRepository<IResource>
+{
+    private readonly List<IResource> models = new List<IResource>();
+
+    public IReadOnlyCollection<IResource> Models => this.models.AsReadOnly();
+
+    public void Add(IResource model)
+    {
+        this.models.Add(model);
+    }
+
+    public IResource TakeOne(string modelName)
+    {
+        return this.models.FirstOrDefault(x => x.Name == modelName);
+    }
+}
