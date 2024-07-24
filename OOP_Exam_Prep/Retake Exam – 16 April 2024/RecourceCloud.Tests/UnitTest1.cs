c0b7f09da1b4d020d219611356280b46b51d2745
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-24 15:03:13 +0300

    tests

---
+++ OOP_Exam_Prep/Retake Exam – 16 April 2024/RecourceCloud.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RecourceCloud.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
