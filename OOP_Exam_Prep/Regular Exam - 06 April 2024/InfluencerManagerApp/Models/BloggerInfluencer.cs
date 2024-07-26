5a2ebca742b7a265cffa36fec35847be4407df71
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-26 20:12:30 +0300

    BlogInfluencer

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Models/BloggerInfluencer.cs
@@ -0,0 +1,17 @@
+﻿namespace InfluencerManagerApp.Models;
+
+public class BloggerInfluencer : Influencer
+{
+    private const double ENGAGEMENT_RATE = 2.0;
+    private const double FACTOR = 0.2;
+
+    public BloggerInfluencer(string username, int followers)
+        : base(username, followers, ENGAGEMENT_RATE)
+    {
+    }
+
+    public override int CalculateCampaignPrice()
+    {
+        return (int)(Math.Floor(this.Followers * this.EngagementRate * FACTOR));
+    }
+}
