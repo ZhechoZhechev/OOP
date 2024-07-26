418005122cfe7f7c76c4df236daf09d66e7248bf
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-26 20:02:56 +0300

    BussinessInfluencer

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Models/BusinessInfluencer.cs
@@ -0,0 +1,16 @@
+﻿namespace InfluencerManagerApp.Models;
+
+public class BusinessInfluencer : Influencer
+{
+    private const double ENGAGEMENT_RATE = 3.0;
+    private const double FACTOR = 0.15;
+
+    public BusinessInfluencer(string username, int followers) : base(username, followers, ENGAGEMENT_RATE)
+    {
+    }
+
+    public override int CalculateCampaignPrice()
+    {
+        return (int)(Math.Floor(this.Followers * this.EngagementRate * FACTOR));
+    }
+}
