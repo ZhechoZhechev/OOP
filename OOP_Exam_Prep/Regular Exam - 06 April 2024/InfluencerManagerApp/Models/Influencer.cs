5186708196e209c7cee95299a002fbc30f5fb7c0
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-27 12:21:39 +0300

    roll back

--- OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Models/Influencer.cs
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Models/Influencer.cs
@@ -1,82 +1,74 @@
-﻿using InfluencerManagerApp.Models.Contracts;
+﻿namespace InfluencerManagerApp.Models;
 
-namespace InfluencerManagerApp.Models
+using InfluencerManagerApp.Models.Contracts;
+using InfluencerManagerApp.Utilities.Messages;
+using System.Collections.Generic;
+
+public abstract class Influencer : IInfluencer
 {
-    public abstract class Influencer : IInfluencer
+    private string username;
+    private int followers;
+    private double engagementRate;
+    private double income;
+    private readonly List<string> participations;
+
+    protected Influencer(string username, int followers, double engagementRate)
     {
-        private string username;
-        private int followers;
-        private double engagementRate;
-        private double income;
-        private readonly List<string> participations;
+        this.Username = username;
+        this.Followers = followers;
+        this.EngagementRate = engagementRate;
 
-        protected Influencer(string username, int followers, double engagmentRate)
+        this.participations = new List<string>();
+        this.Income = 0;
+    }
+
+    public string Username
+    {
+        get => this.username;
+        private set
         {
-            Username = username;
-            Followers = followers;
-            EngagementRate = engagmentRate;
-
-            Income = 0;
-            participations = new List<string>();
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException(ExceptionMessages.UsernameIsRequired);
+            this.username = value;
         }
+    }
 
-        public string Username
+    public int Followers
+    {
+        get => this.followers;
+        private set
         {
-            get => username;
-            private set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    throw new ArgumentException($"Username is required.");
-                }
-                username = value;
-            }
+            if (value < 0)
+                throw new ArgumentException(ExceptionMessages.FollowersCountNegative);
+            this.followers = value;
         }
+    }
 
-        public int Followers
-        {
-            get => followers;
-            private set
-            {
-                if (value < 0)
-                {
-                    throw new ArgumentException($"Followers count cannot be a negative number.");
-                }
-                followers = value;
-            }
-        }
+    public double EngagementRate { get; private set; }
 
-        public double EngagementRate
-        {
-            get => engagementRate;
-            private set
-            {
-                engagementRate = value;
-            }
-        }
+    public double Income { get; private set; }
 
-        public double Income
-        {
-            get => income;
-            private set
-            {
-                income = value;
-            }
-        }
+    public IReadOnlyCollection<string> Participations => this.participations.AsReadOnly();
 
-        public IReadOnlyCollection<string> Participations => participations;
+    public abstract int CalculateCampaignPrice();
 
-        public void EarnFee(double amount) => income += amount;
+    public void EarnFee(double amount)
+    {
+        this.income += amount;
+    }
 
-        public void EnrollCampaign(string brand) => participations.Add(brand);
+    public void EndParticipation(string brand)
+    {
+        this.participations.Remove(brand);
+    }
 
-        public abstract int CalculateCampaignPrice();
+    public void EnrollCampaign(string brand)
+    {
+        this.participations.Add(brand);
+    }
 
-        public override string ToString() => $"{Username} - Followers: {Followers}, Total Income: {Income}";
-
-        public void EndParticipation(string brand)
-        {
-            participations.Remove(brand);
-        }
+    public override string ToString() 
+    {
+        return $"{Username} - Followers: {Followers}, Total Income: {Income}";
     }
 }
