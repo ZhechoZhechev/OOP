1590a8e16290031635e9ddb2ab6b269beae9ccf6
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-27 11:55:15 +0300

    CampaignRepository

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Repositories/CampaignRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace InfluencerManagerApp.Repositories;
+
+using InfluencerManagerApp.Models;
+using InfluencerManagerApp.Repositories.Contracts;
+
+public class CampaignRepository : IRepository<Campaign>
+{
+    private readonly List<Campaign> models;
+    public IReadOnlyCollection<Campaign> Models => this.models.AsReadOnly();
+
+    public void AddModel(Campaign model)
+    {
+        this.models.Add(model);
+    }
+
+    public Campaign FindByName(string name)
+    {
+        return this.models.FirstOrDefault(i => i.Brand == name)!;
+    }
+
+    public bool RemoveModel(Campaign model)
+    {
+        return this.models.Remove(model);
+    }
+}
