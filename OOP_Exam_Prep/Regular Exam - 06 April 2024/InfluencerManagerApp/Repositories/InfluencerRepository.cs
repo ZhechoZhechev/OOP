b25252a04a4a6eddb72d9e733a70247a4d779bfb
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-27 12:00:23 +0300

    change

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Repositories/InfluencerRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace InfluencerManagerApp.Repositories;
+
+using InfluencerManagerApp.Models;
+using InfluencerManagerApp.Repositories.Contracts;
+
+public class InfluencerRepository : IRepository<Influencer>
+{
+    private readonly List<Influencer> models;
+
+    public IReadOnlyCollection<Influencer> Models => models.AsReadOnly();
+
+    public void AddModel(Influencer model)
+    {
+        models.Add(model);
+    }
+
+    public Influencer FindByName(string name)
+    {
+        return models.FirstOrDefault(i => i.Username == name)!;
+    }
+
+    public bool RemoveModel(Influencer model)
+    {
+        return models.Remove(model);
+    }
+}
