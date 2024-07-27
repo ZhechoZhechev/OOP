b0ef3627382e8d4073809c74f89120593f000686
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-27 11:46:31 +0300

    ServiceCampaign

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/InfluencerManagerApp/Models/ServiceCampaign.cs
@@ -0,0 +1,11 @@
+﻿namespace InfluencerManagerApp.Models;
+
+public class ServiceCampaign : Campaign
+{
+    private const double BUDGET = 30000;
+
+    public ServiceCampaign(string brand)
+        : base(brand, BUDGET)
+    {
+    }
+}
