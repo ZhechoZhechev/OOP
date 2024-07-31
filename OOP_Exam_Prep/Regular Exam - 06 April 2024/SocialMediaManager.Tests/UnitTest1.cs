ee9db6aff9363ca2eebbfa789552fc2b3e06cd5f
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-31 13:56:13 +0300

    test skeleton

---
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/SocialMediaManager.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SocialMediaManager.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
