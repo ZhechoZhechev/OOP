441232eb021381c1f7c4caba5c9c8d15ad7236e6
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-31 14:51:31 +0300

    RemoveInfluencerThrowsIfNameNullOrWhiteSpace

--- OOP_Exam_Prep/Regular Exam - 06 April 2024/SocialMediaManager.Tests/InfluencerRepoTests.cs
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/SocialMediaManager.Tests/InfluencerRepoTests.cs
@@ -17,14 +17,14 @@ public class Tests
     {
         Influencer influencer = null;
 
-        var msg = Assert.Throws <ArgumentNullException>(() => this.inflRepository.RegisterInfluencer(influencer));
+        var msg = Assert.Throws<ArgumentNullException>(() => this.inflRepository.RegisterInfluencer(influencer));
 
         Assert.That(msg.Message, Does.Contain("Influencer is null"));
         Assert.That(msg.ParamName, Is.EqualTo(nameof(influencer)));
     }
 
     [Test]
-    public void RegisterInfluencerThrowsIfNameExists() 
+    public void RegisterInfluencerThrowsIfNameExists()
     {
         inflRepository.RegisterInfluencer(influencer);
 
@@ -42,6 +42,19 @@ public class Tests
         {
             Assert.That(this.inflRepository.Influencers.Count > 0);
             Assert.That(msg, Is.EqualTo($"Successfully added influencer {influencer.Username} with {influencer.Followers}"));
+        });
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    public void RemoveInfluencerThrowsIfNameNullOrWhiteSpace(string username)
+    {
+        var msg = Assert.Throws<ArgumentNullException>(() => this.inflRepository.RemoveInfluencer(username));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(msg.ParamName, Is.EqualTo(nameof(username)));
+            Assert.That(msg.Message, Does.Contain("Username cannot be null"));
         });
     }
 }
