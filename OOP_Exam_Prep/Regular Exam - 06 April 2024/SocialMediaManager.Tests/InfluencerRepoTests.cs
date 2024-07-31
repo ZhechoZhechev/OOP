7e560154e8280f69b71d6318386576b54f66ed13
ZhechoZhechev <mishomishev024@gmail.com>
2024-07-31 14:30:39 +0300

    RegisterInfluencerThrowsIfNameExists

--- OOP_Exam_Prep/Regular Exam - 06 April 2024/SocialMediaManager.Tests/InfluencerRepoTests.cs
+++ OOP_Exam_Prep/Regular Exam - 06 April 2024/SocialMediaManager.Tests/InfluencerRepoTests.cs
@@ -20,4 +20,18 @@ public class Tests
         Assert.That(msg.Message, Does.Contain("Influencer is null"));
         Assert.That(msg.ParamName, Is.EqualTo(nameof(influencer)));
     }
+
+    [Test]
+    public void RegisterInfluencerThrowsIfNameExists() 
+    {
+        Influencer influencer = new Influencer("Test", 1);
+        inflRepository.RegisterInfluencer(influencer);
+
+        var msg = Assert.Throws<InvalidOperationException>(() => this.inflRepository.RegisterInfluencer(influencer));
+
+        Assert.That(msg.Message, Is.EqualTo($"Influencer with username {influencer.Username} already exists"));
+    }
+
+    [Test]
+    public void 
 }
